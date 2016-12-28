d70a1c09b4d4ef74ee6595527a9de46a7aff250a
KU <ku@digitaldolphins.jp>
2016-12-28 15:29:26 +0900

    移動

--- RedirAx/RedirAx.h
+++ RedirAx/RedirAx.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Wed Dec 28 15:08:36 2016
+/* at Wed Dec 28 15:28:28 2016
  */
 /* Compiler settings for .\RedirAx.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
